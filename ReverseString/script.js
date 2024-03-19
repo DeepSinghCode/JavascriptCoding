3df0542de71423976f3557d0fb4dc2e3f1d9dcf9
DeepSinghCode <deepsangeet2001@gmail.com>
2024-03-18 22:12:14 -0400

    update the method

--- ReverseString/script.js
+++ ReverseString/script.js
@@ -1,2 +1,12 @@
-var i = 2 +3;
-document.write("revese string is: ", i);
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example usage:
+const originalString = 'Hello, world!';
+const reversedString = reverseString(originalString);
+
+document.write("Given string is: ", originalString);
+
+document.write("Revese string is: ", reversedString);
+
